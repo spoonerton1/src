b46ec5fd1c40f7237b3895c5d64261e345feea01
spoonerton1 <99636226+spoonerton1@users.noreply.github.com>
2022-02-17 15:19:03 +0900

    Add files via upload

---
+++ 2-17.py
@@ -0,0 +1,39 @@
+# import random
+
+# rand_num = random.randrange(1, 100)
+
+# num = 0
+
+# while rand_num != num:
+
+#     num = int(input("숫자를 입력하세요")) 
+
+#     if num  > rand_num:
+#         print("DOWN")
+    
+#     elif num < rand_num:
+#         print("UP")
+
+# print("정답!")
+
+#------------------------------------------------
+
+import random
+
+def generate_nums():
+    rand_1 = random.randrange(1, 10)
+    rand_2 = random.randrange(1, 10)
+    while rand_1 == rand_2:
+        rand_2 = rand.randrange(1, 10)
+    rand_3 = random.randrange
+
+def check_num(in_1, in_2, in_3):
+
+yn = input("play game? (Y/N)")
+
+while yn == "Y":
+    is_continue = True
+    game_step = 1
+    while is_continue:
+        print(f**BASEBALL GAME ... ROUND : {game step})
+        in_1 = input("")

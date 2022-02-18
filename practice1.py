5c899f0998a04e3c34b1873a7e47ac529799e11c
spoonerton1 <99636226+spoonerton1@users.noreply.github.com>
2022-02-18 16:06:39 +0900

    Add files via upload

--- practice1.py
+++ practice1.py
@@ -1,4 +1,97 @@
-a = input("숫자를 입력: ")
-a_list = list(a)
-a_list.reverse()
-print(a)
+
+
+
+# # # # print("끝말잇기 시작")
+# # # # word = []
+# # # # word.append(input("첮 단어를 말해라: "))
+# # # # print(f"{words[0]}")
+
+# # # # def check_word(_words, _now_word):
+# # # #     for i in range(len(_words)):
+# # # #         if _now_word == _words[i]:
+# # # #             print("이미 사용한 단어입니다!")
+
+# # # # is_alive = true
+# # # # while is_alive:
+# # # #     now_input = input("다음 단어를 말해라: ")
+
+# # # #     if check_word(a, now_input):
+# # # #         now_input.append(now_input)
+# # # #         word.append(now_input)
+# # # #         print(word)
+
+
+
+# # # # while a[-1] != a[0]:
+# # # #     a = input("")
+
+
+# # # # print("떙!")
+
+# # # # _____________________
+
+
+
+# # # item_dict = {
+# # #     "아이폰6": (3, 3),
+# # #     "갤럭시7" : (5,4),
+# # #     "갤노트7" : (2,1),
+# # #     "v20": (4, 4),
+# # # }
+
+
+# # # for key, item in item_dict.item():
+# # #     if item[] >= 4 and item[1] >= 4:
+
+
+
+# # # lines = input("")
+# # # lines = lines
+
+# # print("")
+# # print("")
+# # print("")
+
+# # for this_word in str(lines):
+# #     this_word_count = 0
+# #     for other_word in lines:
+# #         if other_word == this_word:
+# #             this_word_count += 1
+# #         if this_word_count >= 1:
+# #             print(f"{this_word} {this_word_count}",
+# # #             end=" ")"
+
+# # # print("")
+# # # # ___________________________-
+
+# # input_n = _input()
+# # def check_n(_input_n)
+# # result = set)
+# # for i in _input_n:
+# #     if i not in set("12345"):
+# #         result.add(i)
+
+# #     return result
+
+
+# # all_num = [i for i in range(2, 100000+1)]
+
+# # selected_num = []
+# # while len(all_num > 0):
+# #     now_num = all_num[0]
+# #     selected_num.append(now_num)
+# #     for i in all_num:
+# #         if i % now_num == 0:
+# #             all_num.remove(i)
+
+# # print(selected_num)
+
+
+
+
+# input_n = input()
+# tmp =  ""
+# for i in reversed(input_n):
+#     tmp += i
+
+# print(f"sum is {int(input_n) + int(tmp)}")
